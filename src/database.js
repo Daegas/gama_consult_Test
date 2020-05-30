d90c9d1154b685e206aac4b032e2f25cd6b33a81
SandraPAM <sandra_mely22@hotmail.es>
2020-05-29 23:49:56 -0500

    DB settings

---
+++ src/database.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql');
+
+const { database } = require('./keys'); //All the secret stuffs
+const {promisify} = require('util');
+
+const pool = mysql.createPool(database); //Creat connection
+
+pool.getConnection((err, conn) => {
+    if (err) {
+        //MOST COMMON ERRORS
+        if(err.code == 'PROTOCOL_CONNECTION_LOST') {
+            console.error('DATABASE CONNECTION CLOSED');
+        }
+        if(err.code == "ER_CON_COUNT_ERROR") {
+            console.error('DATABASE HAS TO MANY CONNECTIONS');
+        }
+        if(err.code === "ECONNREFUSED") { //MAINLY DUE TO WRONG CREDENTIAL DATA
+            console.error('DATABASE CONNECTION WAS REFUSED');
+        }
+    }
+
+    if (conn) {
+        conn.release(); 
+        console.log('DB CONNECTED: ', database.database);
+    }
+
+    return;
+});
+
+
+
+//Promiseify Queries
+pool.query = promisify(pool.query); //Enable 'async-await' and 'promisses' - for no async behaivour
+
+module.exports = pool; //Export pool connection
