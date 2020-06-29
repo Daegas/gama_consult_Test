90a7ae9d5b70aec064afc62b498226fd155bca5c
SandraPAM <sandra_mely22@hotmail.es>
2020-06-29 12:20:28 -0500

    Botones Edit y Delete funcionando con Server-Processing. Vista del botón 'Nuevo' dentro de plantilla DataTables

--- src/public/js/medicine/list.js
+++ src/public/js/medicine/list.js
@@ -72,6 +72,15 @@ function editForm( med ) {
 
 $(document).ready(function() {
     var table = $('#tbMeds').DataTable( {
+        dom: '<"top row" l <"toolbar mx-auto"> frt><"bottom"ip>', //Se agrega clase 'toolbar' a la plantilla
+        fnInitComplete: function(){ //Función para desplegar contenido en div.toolbar
+            html = `
+            <button class="btn btn-dark rounded">
+            <a href="/meds/add"><i class="fa fa-plus-circle"></i> Nuevo</a>
+            </button>
+            `;
+           $('div.toolbar').html(html);
+        },
         "paging":   true,
         "ordering": true,
         "info":     true,
@@ -91,39 +100,65 @@ $(document).ready(function() {
             {"data": "P_Proveedor"},
             {"data": "P_Publico"},
             {"data": "Descuento"},
-            {"data": "Caducidad"},
+            {"data": "Caducidad",
+                "render": function(data,  type, row, meta) {
+                    let formatDate = data.split("T")[0];
+                    let html = `<span class="render-timeago">${formatDate}</span>`;
+                    return html;
+                }
+            },
+            {   //Edit button
+                "data": null,
+                "orderable":      false,
+                "className":      'details-control',
+                "render": function ( data, type, row, meta ) {
+                    let html = `<i data-MedicamentoID=${data.MedicamentoID}
+                    class="fa fa-pencil text-info pointer" title="Editar"></i>`;
+                    return html;
+                }
+            },
+            {   //Delete button
+                "data": null,
+                "orderable":    false,
+                "render": function ( data, type, row, meta ) {
+                    let html = `<a href="/meds/delete/${data.MedicamentoID}">
+                                    <i class="fa fa-trash text-danger" title="Eliminar"></i>
+                                </a>`;
+                    return html;
+                }
+            }
         ]
     } );
+    /* EDIT FORM */
+    // Add event listener for opening and closing 'Edit form'
+    $('#tbMeds tbody').on('click', 'td.details-control', function () {
+        let this_ = $(this);
+        var tr = this_.closest('tr');
+        var row = table.row( tr );
 
-    // // Add event listener for opening and closing details
-    // // $('#tbMeds tbody').on('click', 'td.details-control', function () {
-    //     // let this_ = $(this);
-        // // var tr = this_.closest('tr');
-        // // var row = table.row( tr );
-
-        // // let iconElement = this_.find('a').find('i');
+        let iconElement = this_.find('i');
         
-        // // if ( row.child.isShown() ) {
-        //     // // This row is already open - close it
-            // // row.child.hide();
-        //     // iconElement.attr('title','Editar').removeClass('fa-window-close').addClass('fa-pencil');
-        // // }
-        // // else {
-        //     // // Open this row
-            // // let MedicamentoID = tr.attr('data-MedicamentoID');
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            row.child.hide();
+            iconElement.attr('title','Editar').removeClass('fa-window-close').addClass('fa-pencil');
+        }
+        else {
+            // Open this row
+            let MedicamentoID = iconElement.attr('data-MedicamentoID');
 
-            // // var xhttp = new XMLHttpRequest();
-            // // xhttp.open('GET', '/meds/edit/'+MedicamentoID);
-            // // xhttp.onload = function(){
-            //     // var med = xhttp.responseText;
-                // // med = JSON.parse(med);
+            var xhttp = new XMLHttpRequest();
+            xhttp.open('GET', '/meds/edit/'+MedicamentoID);
+            xhttp.onload = function(){
+                var med = xhttp.responseText;
+                med = JSON.parse(med);
 
-                // Show data in a new row(child)
-            //     // row.child( editForm(med.med) ).show();
-            // // };
-            // // xhttp.send();
+                //Show data in a new row(child)
+                row.child( editForm(med.med) ).show();
+            };
+            xhttp.send();
 
-        //     // iconElement.attr('title', 'Cancelar').removeClass('fa-pencil').addClass('fa-window-close');
-    //     // }
-//     // } );
+            iconElement.attr('title', 'Cancelar').removeClass('fa-pencil').addClass('fa-window-close');
+        }
+    } );
  });
